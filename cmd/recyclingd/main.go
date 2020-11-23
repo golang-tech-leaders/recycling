59cdadeed5a3652900d96ac2c37dde917a737502
khdaniel <daniel.khasanov@iqoption.com>
2020-11-23 18:15:27 +0300

    New pipeline

--- cmd/recyclingd/main.go
+++ cmd/recyclingd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"recycling/internal/config"
 	"recycling/internal/platform/database"
 	"recycling/internal/platform/server"
@@ -8,6 +9,7 @@ import (
 
 func main() {
 	cfg := config.PrepareConfig()
+	fmt.Println("DATABASE ENV IS: " + cfg.DbAddress)
 	db := database.NewPostgresWasteStorage(cfg)
 	db.Migrate()
 	srv := server.NewServer(cfg, db)
