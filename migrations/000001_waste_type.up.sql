5262ded773392122858971208bf3033e26e359cb
khdaniel <daniel.khasanov@iqoption.com>
2020-12-27 23:11:13 +0300

    waste type synced

--- migrations/000001_waste_type.up.sql
+++ migrations/000001_waste_type.up.sql
@@ -7,13 +7,14 @@ CREATE TABLE waste_type(
 
 INSERT INTO waste_type(id, name) VALUES ('стекло', 'стекло');
 INSERT INTO waste_type(id, name) VALUES ('пластик', 'одноразовый пластик');
-INSERT INTO waste_type(id, name) VALUES ('метал', 'отходы из метала');
 INSERT INTO waste_type(id, name) VALUES ('одежда', 'одежда');
 INSERT INTO waste_type(id, name) VALUES ('иное', 'иное');
-INSERT INTO waste_type(id, name) VALUES ('опасное', 'опасные отходы');
+INSERT INTO waste_type(id, name) VALUES ('опасныеотходы', 'опасные отходы');
 INSERT INTO waste_type(id, name) VALUES ('батарейки', 'батарейки, аккумуляторы, АКБ');
 INSERT INTO waste_type(id, name) VALUES ('лампочки', 'лампы, лампы дневного света');
-INSERT INTO waste_type(id, name) VALUES ('техника', 'бытовая техника');
-INSERT INTO waste_type(id, name) VALUES ('тетрапак', 'тара/упаковка tetra pack');
+INSERT INTO waste_type(id, name) VALUES ('бытоваятехника', 'бытовая техника');
+INSERT INTO waste_type(id, name) VALUES ('тетрапак', 'тетра пак');
 INSERT INTO waste_type(id, name) VALUES ('крышечки', 'пластиковые крышечки');
 INSERT INTO waste_type(id, name) VALUES ('шины', 'шины резиновые');
+INSERT INTO waste_type(id, name) VALUES ('бумага', 'бумага');
+INSERT INTO waste_type(id, name) VALUES ('металл', 'металл');
